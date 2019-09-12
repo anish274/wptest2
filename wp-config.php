71852890475b13c0878f0f14352eaebb144e9ecf
anish <anish.web1@gmail.com>
2019-09-12 08:44:13 +0530

    change env variables. this should work.

--- wp-config.php
+++ wp-config.php
@@ -38,10 +38,10 @@ define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
 // add following variables secret variables
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_NAME', $_ENV['WP_DB_NAME']);
+define('DB_USER', $_ENV['WP_DB_USER']);
+define('DB_PASSWORD', $_ENV['WP_DB_PASSWORD']);
+define('DB_HOST', $_ENV['WP_DB_HOST']);
 
 
 /**
